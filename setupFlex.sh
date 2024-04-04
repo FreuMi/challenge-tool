7a4ecc91380beb2e0611f7c7de76de55acea8e9e
Michael Freund <michael.freund@iis.fraunhofer.de>
2024-04-04 11:31:49 +0200

    update script

--- setupFlex.sh
+++ setupFlex.sh
@@ -4,19 +4,19 @@
 SOURCE_DIR="./eswc-kgc-challenge-2024/track2/"
 
 # Destination directory
-DEST_DIR="./downloads/track2/"
+DEST_DIR="./downloads/eswc-kgc-challenge-2024/track2/"
 
 # Use rsync to merge the directories
 rsync -avh --progress "$SOURCE_DIR" "$DEST_DIR"
 echo "Merge completed."
 
 # Remove GTFS folders with heterogeneity
-rm -r "./downloads/track2/gtfs-madrid-bench/heterogeneity_files/"
-rm -r "./downloads/track2/gtfs-madrid-bench/heterogeneity_mixed/"
-rm -r "./downloads/track2/gtfs-madrid-bench/heterogeneity_nested/"
-rm -r "./downloads/track2/gtfs-madrid-bench/heterogeneity_tabular/"
+rm -r "./downloads/eswc-kgc-challenge-2024/track2/gtfs-madrid-bench/heterogeneity_files/"
+rm -r "./downloads/eswc-kgc-challenge-2024/track2/gtfs-madrid-bench/heterogeneity_mixed/"
+rm -r "./downloads/eswc-kgc-challenge-2024/track2/gtfs-madrid-bench/heterogeneity_nested/"
+rm -r "./downloads/eswc-kgc-challenge-2024/track2/gtfs-madrid-bench/heterogeneity_tabular/"
 
 # Remove Track1
-rm -r "./downloads/track1/"
+rm -r "./downloads/eswc-kgc-challenge-2024/track1/"
 
 echo "Directories deleted."
